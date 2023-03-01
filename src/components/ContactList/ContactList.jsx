e0bd6792960b86ee94d2ec47fabe3593fd890ec3
Anatoliy Pyrkhavka <Anatoliy23@i.ua>
2023-03-01 22:11:45 +0200

    Готовий

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,29 +1,28 @@
 import s from './ContactList.module.css';
-import PropTypes from 'prop-types';
 import ContactItem from '../ContactItem/ContactItem';
+import { useSelector } from 'react-redux';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
+const getVisibleContacts = (items, filter) =>
+  items.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+const ContactList = () => {
+  const items = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter);
+  const contacts = getVisibleContacts(items, filter);
+
+  return (
     <ul className={s.list}>
-        {contacts.map(contacts => (
-            <ContactItem
-                key={contacts.id}
-                id={contacts.id}
-                name={contacts.name}
-                number={contacts.number}
-                onDeleteContact={onDeleteContact}
-            />
-        ))}
+      {contacts.length ? (
+        contacts.map(({ id, name, number }) => (
+          <ContactItem key={id} id={id} name={name} number={number} />
+        ))
+      ) : (
+        <p className={s.text}>No contacts</p>
+      )}
     </ul>
-);
-ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    ),
-    onDeleteContact: PropTypes.func.isRequired,
+  );
 };
 
 export default ContactList;
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,11 +2,11 @@ import s from './Notification.module.css';
 import PropTypes from 'prop-types';
 
 const Notification = ({ message }) => {
-    return <p className={s.title}>{message}</p>;
+  return <p className={s.title}>{message}</p>;
 };
 
 Notification.propTypes = {
-    message: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
 };
 
 export default Notification;
